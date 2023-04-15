39421c42d821d1ec684e125918d350959aba2cd5
devashish234073 <devashish234073@gmail.com>
2023-04-15 09:03:31 +0530

    calling one api from another

--- src/main/java/com/test/xraydemo1/TestController.java
+++ src/main/java/com/test/xraydemo1/TestController.java
@@ -1,18 +1,28 @@
 package com.test.xraydemo1;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class TestController {
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
 	@GetMapping("/api")
 	public ResponseEntity<String> getMsg(@RequestParam("code") int code) {
 		if(code==200) {
-			String msg = postMessage("test app").getBody();
+			//String msg = postMessage("test app").getBody();
+			String msg = restTemplate.postForObject(
+			         "http://localhost:8080/postapi/v3", "test", String.class);
 			return ResponseEntity.ok().body(String.format("SUCCESS [%s]",msg)) ;
 		} else {
 			if(code>=400 && code<499) {
@@ -40,8 +50,8 @@ public class TestController {
 	public ResponseEntity<String> getMsgV3(@RequestParam("code") int code) {
 		if(code==200) {
 			return ResponseEntity.ok().body("--v3--SUCCESS");
-		} else {
-			if(code>=400 && code<499) {
+		} else { 
+			if(code>=400 && code<499) { 
 				return ResponseEntity.badRequest().body("--v3--bad req code:"+code);
 			} else {
 				return ResponseEntity.internalServerError().body("--v3--error");
@@ -51,7 +61,7 @@ public class TestController {
 	
 	@PostMapping("/postapi/v3")
 	public ResponseEntity<String> postMessage(@RequestBody String name) {
-		return ResponseEntity.badRequest().body("Hello "+name);
+		return ResponseEntity.ok().body("Hello "+name);
 	}
 	
 	@GetMapping("/api2/v1")
